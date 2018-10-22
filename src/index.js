259115aa17e52e216a86951775f8fad8b2e9f78b
francisvelueta <fvelueta@outlook.com>
2018-10-21 20:14:20 -0500

    add auth with firebase and material-ui

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter, Route } from 'react-router-dom';
+import SignIn from './components/SignIn'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<BrowserRouter basename = { process.env.PUBLIC_URL } >
+    <div>
+            <Route exact path = '/' component = { App } />
+            <Route  path = '/signin' component = { SignIn } />
+
+      </div>
+    </BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
