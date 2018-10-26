80f73742cd389a7dba4ad32afb8ddccf219b2a40
Alterna87 <fvelueta@outlook.com>
2018-10-26 12:46:27 +0000

    Updates

---
+++ precache-manifest.31be7c30991cfb803fb435fec4256950.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6110903587f4163e1f121a25a82a6295",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/logo.61109035.png"
+  },
+  {
+    "revision": "db3000449ee3cfd6d8bb",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/runtime~main.db300044.js"
+  },
+  {
+    "revision": "e492f9acb4c71f4db699",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/main.e492f9ac.chunk.js"
+  },
+  {
+    "revision": "04ebd596c9ba235580dd",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/1.04ebd596.chunk.js"
+  },
+  {
+    "revision": "e492f9acb4c71f4db699",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/css/main.409237f8.chunk.css"
+  },
+  {
+    "revision": "75965f3ca7fbaac65156f8bc278ef37d",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/index.html"
+  }
+];
